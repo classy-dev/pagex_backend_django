4f213c7e937b17e1e2e560a179726e8517e0006f
classy-dev <gwolf.bond97@gmail.com>
2020-03-16 01:54:43 +0530

    image upload path updated

--- author/models.py
+++ author/models.py
@@ -12,7 +12,7 @@ class Passion(models.Model):
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return 'user_images/user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Profile(models.Model):
