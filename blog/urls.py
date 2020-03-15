520f2cedb773c2f65ed66ea8b5a5e6354e5d4b07
classy-dev <gwolf.bond97@gmail.com>
2020-03-16 01:59:40 +0530

    blog list api added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-
+from django.urls import path
 from blog import views
 
 drf_router = routers.DefaultRouter()
 drf_router.register('my-post', views.MyBlogPostViewSet, basename='my-blog-post')
 
 urlpatterns = [
+                  path('feed/', views.BlogPostFeedAPI.as_view(), name='blog-feed'),
               ] + drf_router.urls
